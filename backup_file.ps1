002d77e10767714e6291032e69f91011db8c935b
Austin Decker <deckaj01@pfw.edu>
2022-03-26 17:08:09 -0400

    added safety and checks

--- backup_file.ps1
+++ backup_file.ps1
@@ -4,8 +4,18 @@ Param(
     [Parameter(Mandatory, HelpMessage = "please provide a destination directory")]
     $DestDir
 )
-
+if(-Not (Test-Path $SrcDir))
+{
+    Throw "the source directory was invalid or does not exist."
+}
 $BackUpDate = Get-Date -Format "MM-dd-yy"
+$DestFile = "$($DestDir + 'backup-' + $BackUpDate + '.zip')"
+
+if(Test-Path $DestFile)
+{
+    rmdir -Recurse -Force $DestFile
+}
 Compress-Archive -Path $SrcDir -CompressionLevel 'Fastest' -DestinationPath "$($DestDir + 'backup-' + $BackUpDate)"
 Write-Host "file backup successfully created at $($DestDir + 'backup-' + $BackUpDate)"
+
 
