30c1117de31ec7ec18d71dadf90f1b890a4821bf
Austin Decker <deckaj01@pfw.edu>
2022-03-26 16:42:05 -0400

    fixed destination file placement

--- backup_file.ps1
+++ backup_file.ps1
@@ -1,11 +1,11 @@
 Param(
     [Parameter(Mandatory, HelpMessage = "please provide a source directory")]
-    [string]$SrcDir,
+    $SrcDir,
     [Parameter(Mandatory, HelpMessage = "please provide a destination directory")]
-    [string]$DestDir
+    $DestDir
 )
 
 $BackUpDate = Get-Date -Format "MM-dd-yy"
 Compress-Archive -Path $SrcDir -CompressionLevel 'Fastest' -DestinationPath "$($DestDir + 'backup-' + $BackUpDate)"
-Write-Host "file backup successfully created at $DestDir"
+Write-Host "file backup successfully created at $($DestDir + 'backup-' + $BackUpDate)"
 
