0dedab8f628363ef1a70649ec8e89b0f6067ef63
Austin Decker <deckaj01@pfw.edu>
2022-03-27 13:54:48 -0400

    added a check for the existence of the dest dir

--- backup.ps1
+++ backup.ps1
@@ -1,13 +1,19 @@
 Param(
     [Parameter(Mandatory, HelpMessage = "please provide a source directory")]
-    $SrcDir,
+    [string]$SrcDir,
     [Parameter(Mandatory, HelpMessage = "please provide a destination directory")]
-    $DestDir
+    [string]$DestDir
 )
 if(-Not (Test-Path $SrcDir))
 {
     Throw "the source directory was invalid or does not exist."
 }
+
+if(-Not (Test-Path $DestDir))
+{
+    throw "the destination directory was invalid or does not exist."
+}
+
 $BackUpDate = Get-Date -Format "MM-dd-yy"
 $DestFile = "$($DestDir + 'backup-' + $BackUpDate + '.zip')"
 
